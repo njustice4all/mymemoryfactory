8e5ac876f2207f59ece7acb229e9d0f003e8be7e
YoungJoo Han <urdlove@gmail.com>
2018-04-03 11:37:27 +0900

    Implements fuctional logic
    - 다르게 구현해야함

--- Javascript/reduce.js
+++ Javascript/reduce.js
@@ -50,3 +50,27 @@ const cal = (value, index, array) => {
 }
 
 cal(0, 0, numbers);
+
+
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]이란 배열을 만든다.
+// 이 배열의 각 요소들을 x3한 배열을 구한다.
+// x3한 배열에서 2의 배수만 추려내서 새로 배열을 구한다.
+// 2의 배수들만 모아놓은 각 요소들을 합한 결과를 구하자.
+
+const empty10Array = new Array(10);
+const numbers = empty10Array.fill(undefined).map((value, index) => index + 1);
+const x3Array = numbers.map(value => value * 3);
+const evenNumbers = x3Array.filter(value => value % 2 === 0);
+const sum = evenNumbers.reduce((accumulator, value) => accumulator + value);
+
+const fillNumbers = array => array.fill(undefined).map((value, index) => index + 1);
+const x3 = array => array.map(value => value * 3);
+const getEvenNumbers = array => array.filter(value => value % 2 === 0);
+const getSum = array => array.reduce((accumulator, value) => accumulator + value);
+
+const targets = [empty10Array, fillNumbers, x3, getEvenNumbers, getSum];
+
+const result = targets.reduce((accumulator, fn) => fn(accumulator));
+
+console.log(result);
+
